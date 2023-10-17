5c8bb5f05aa601e4d7ea93a4b25f5f735812eb73
ShanaNyaa <ShanaNyaa@users.noreply.github.com>
2023-10-17 13:37:41 +0800

    Placeholder Added To Cards

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -137,18 +137,18 @@ export default function Home({
                 <path d="M234-276q51-39 114-61.5T480-360q69 0 132 22.5T726-276q35-41 54.5-93T800-480q0-133-93.5-226.5T480-800q-133 0-226.5 93.5T160-480q0 59 19.5 111t54.5 93Zm246-164q-59 0-99.5-40.5T340-580q0-59 40.5-99.5T480-720q59 0 99.5 40.5T620-580q0 59-40.5 99.5T480-440Zm0 360q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q53 0 100-15.5t86-44.5q-39-29-86-44.5T480-280q-53 0-100 15.5T294-220q39 29 86 44.5T480-160Zm0-360q26 0 43-17t17-43q0-26-17-43t-43-17q-26 0-43 17t-17 43q0 26 17 43t43 17Zm0-60Zm0 360Z"/>
               </svg>
             </Menu.Button>
-              <Menu.Items>
-                <Menu.Item>
-                  {({ active }) => (
-                    <a
-                      className={`${active && 'bg-blue-500'}`}
-                      href="/account-login"
-                    >
-                      Log Masuk
-                    </a>
-                  )}
-                </Menu.Item>
-              </Menu.Items>
+            <Menu.Items>
+              <Menu.Item>
+                {({ active }) => (
+                  <a
+                    className={`${active && 'bg-blue-500'}`}
+                    href="/account-login"
+                  >
+                    Log Masuk
+                  </a>
+                )}
+              </Menu.Item>
+            </Menu.Items>
           </div>
         </Menu>
 
@@ -342,6 +342,12 @@ export default function Home({
               </div>
               <div className="snap-start shrink-0">
                 <div className="rounded-lg bg-white p-3 focus:outline-none">
+                  <p className="text-center font-semibold">
+                    iFIXIT
+                  </p>
+                  <p className="text-center">
+                    Bukit Tinggi, Pahang
+                  </p>
                   <div className="flex flex-col items-center">
                     <div className="rounded-full p-2 mb-2">
                       {/* Image */}
@@ -354,6 +360,12 @@ export default function Home({
               </div>
               <div className="snap-start shrink-0">
                 <div className="rounded-lg bg-white p-3 focus:outline-none">
+                <p className="text-center font-semibold">
+                    iFIXIT
+                  </p>
+                  <p className="text-center">
+                    Bukit Tinggi, Pahang
+                  </p>
                   <div className="flex flex-col items-center">
                     <div className="rounded-full p-2 mb-2">
                       {/* Image */}
@@ -366,6 +378,12 @@ export default function Home({
               </div>
               <div className="snap-start shrink-0">
                 <div className="rounded-lg bg-white p-3 focus:outline-none">
+                <p className="text-center font-semibold">
+                    iFIXIT
+                  </p>
+                  <p className="text-center">
+                    Bukit Tinggi, Pahang
+                  </p>
                   <div className="flex flex-col items-center">
                     <div className="rounded-full p-2 mb-2">
                       {/* Image */}
@@ -378,6 +396,12 @@ export default function Home({
               </div>
               <div className="snap-start shrink-0">
                 <div className="rounded-lg bg-white p-3 focus:outline-none">
+                <p className="text-center font-semibold">
+                    iFIXIT
+                  </p>
+                  <p className="text-center">
+                    Bukit Tinggi, Pahang
+                  </p>
                   <div className="flex flex-col items-center">
                     <div className="rounded-full p-2 mb-2">
                       {/* Image */}
@@ -390,6 +414,12 @@ export default function Home({
               </div>
               <div className="snap-start shrink-0">
                 <div className="rounded-lg bg-white p-3 focus:outline-none">
+                <p className="text-center font-semibold">
+                    iFIXIT
+                  </p>
+                  <p className="text-center">
+                    Bukit Tinggi, Pahang
+                  </p>
                   <div className="flex flex-col items-center">
                     <div className="rounded-full p-2 mb-2">
                       {/* Image */}
@@ -402,6 +432,12 @@ export default function Home({
               </div>
               <div className="snap-start shrink-0">
                 <div className="rounded-lg bg-white p-3 focus:outline-none">
+                <p className="text-center font-semibold">
+                    iFIXIT
+                  </p>
+                  <p className="text-center">
+                    Bukit Tinggi, Pahang
+                  </p>
                   <div className="flex flex-col items-center">
                     <div className="rounded-full p-2 mb-2">
                       {/* Image */}
@@ -414,6 +450,12 @@ export default function Home({
               </div>
               <div className="snap-start shrink-0">
                 <div className="rounded-lg bg-white p-3 focus:outline-none">
+                <p className="text-center font-semibold">
+                    iFIXIT
+                  </p>
+                  <p className="text-center">
+                    Bukit Tinggi, Pahang
+                  </p>
                   <div className="flex flex-col items-center">
                     <div className="rounded-full p-2 mb-2">
                       {/* Image */}
