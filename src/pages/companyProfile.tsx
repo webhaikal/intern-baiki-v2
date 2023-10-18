69b899fb4ba749b42180e3d2004cb0895f2ae5d3
ShanaNyaa <ShanaNyaa@users.noreply.github.com>
2023-10-18 17:52:28 +0800

    Minor Design Update

--- src/pages/companyProfile.tsx
+++ src/pages/companyProfile.tsx
@@ -209,7 +209,7 @@ export default function CompanyProfile({
       <main className="md:mt-4" style={{ backgroundColor: "#F5F5F5" }}>
 
         {/* desktop view */}
-        <div className="mx-auto mb-16 mt-2 hidden max-w-6xl px-4 md:block bg-white">
+        <div className="mx-auto mb-16 mt-2 hidden max-w-6xl px-4 md:block bg-white py-1">
           <div className="flex w-full focus:outline-none">
             <div className="flex px-10 mt-4">
               <a className="text-red-500">
@@ -282,11 +282,22 @@ export default function CompanyProfile({
               Lihat ulasan
             </a>
           </div>
-          <div className="flex w-full focus:outline-none">
-            <div className="flex px-10 mt-4">
-              <a className="text-red-500">
-                test
-              </a>
+          <div className="ml-10 rounded-md mb-16 mt-2 hidden max-w-lg px-4 md:block bg-gray-200">
+            <div className="flex w-full focus:outline-none">
+              <div className="flex mt-4">
+                <svg 
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-6 w-8 fill-black stroke-primary" 
+                  height="24" 
+                  viewBox="0 -960 960 960" 
+                  width="24"
+                >
+                  <path d="M234-276q51-39 114-61.5T480-360q69 0 132 22.5T726-276q35-41 54.5-93T800-480q0-133-93.5-226.5T480-800q-133 0-226.5 93.5T160-480q0 59 19.5 111t54.5 93Zm246-164q-59 0-99.5-40.5T340-580q0-59 40.5-99.5T480-720q59 0 99.5 40.5T620-580q0 59-40.5 99.5T480-440Zm0 360q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q53 0 100-15.5t86-44.5q-39-29-86-44.5T480-280q-53 0-100 15.5T294-220q39 29 86 44.5T480-160Zm0-360q26 0 43-17t17-43q0-26-17-43t-43-17q-26 0-43 17t-17 43q0 26 17 43t43 17Zm0-60Zm0 360Z"/>
+                </svg>
+                <p className="ml-4">
+                  @Username
+                </p>
+              </div>
             </div>
           </div>
         </div>
