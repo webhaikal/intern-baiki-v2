7cf6c997136b050f00c15379ecfba3e2309cb881
ShanaNyaa <ShanaNyaa@users.noreply.github.com>
2023-10-19 16:53:10 +0800

    Added Picture

--- src/pages/companyProfile.tsx
+++ src/pages/companyProfile.tsx
@@ -309,6 +309,7 @@ export default function CompanyProfile({
           </div>
           <div className="flex flex-col">
             {/* Pictures here */}
+            <img src="https://images.unsplash.com/photo-1604999565976-8913ad2ddb7c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=320&h=160&q=80" />
           </div>
         </div>
         
